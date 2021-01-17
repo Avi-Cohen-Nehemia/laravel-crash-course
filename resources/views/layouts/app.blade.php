496435284ef34c69077fbf5d1a93431d92f4d7e8
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2021-01-17 14:42:35 +0000

    added basic nav bar to app view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,23 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, inital-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Posty</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
-    <body>
+    <body class="bg-gray-100">
+        <nav class="p-6 bg-white flex justify-between">
+            <ul class="flex items-center">
+                <li>
+                    <a href="" class="p-3">Home</a>
+                </li>
+                <li>
+                    <a href="" class="p-3">Dashboard</a>
+                </li>
+                <li>
+                    <a href="" class="p-3">Post</a>
+                </li>
+            </ul>
+        </nav>
         @yield('content')
     </body>
 </html>
